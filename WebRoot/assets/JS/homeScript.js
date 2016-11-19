c5e1a82fb92b475c247d585902533ed34690842c
Jordan Mackie <jmackie97@hotmail.com>
2016-11-19 21:24:52 +0000

    started javascript for playername

---
+++ WebRoot/assets/JS/homeScript.js
@@ -0,0 +1,6 @@
+document.getElementById("submitButton").onclick = function() {submitName();
+    function submitName(){
+        var playerName = document.getElementById('playerName').value
+         console.log(playerName);
+    }
+}
