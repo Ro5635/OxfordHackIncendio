132c10590bfe149b6688fbcdba57f9927bdcc596
Jordan Mackie <jmackie97@hotmail.com>
2016-11-19 20:56:52 +0000

    Background image and centering complete

--- MVC/views/pages/home.php
+++ MVC/views/pages/home.php
@@ -10,17 +10,22 @@ include($_SERVER['DOCUMENT_ROOT'].'/../PHPIncludes/Views/pages/home/INC_TopPageG
 
 
 ?>
-        <div class="jumbotron text-center">
-            <img class="img-fluid" src="https://cdn.webaddressgoeshere.com/OxfordHack/media/PlayLogo.png">
-        </div>
-        <div class="container-fluid">
+<div class='container' style='background-image:url("http://media.harrypotter.bloomsbury.com/rep/g/page-background-shell%20-%202.png")'>
             <div class="row">
-                <div class="text-center">
-                    <input id="playerName" type="text">
-                    <button type="submit">Begin.</button>
-                    </div>
+                <div class="container-fluid">
+                    <div class = "col-xs-2"></div>
+                    <img class="img-fluid" src="https://cdn.webaddressgoeshere.com/OxfordHack/media/PlayLogo.png">
+                    <div class= "col-xs-2"></div>
+                </div>
             </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-4">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Your name" /> 
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">Begin.</button>
+                        </span>
+                    </div><!-- /input-group -->
+                </div><!-- /.col-lg-4 -->
+            </div><!-- /.row -->
         </div>
-
-
-<!-- YOUR PAGE HERE!! -->
