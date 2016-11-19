f8fdb1e1fcc41809a89cac25fa86460e90c7159d
Jordan Mackie <jmackie97@hotmail.com>
2016-11-19 22:22:54 +0000

    Attempting to send username to server

--- WebRoot/assets/JS/homeScript.js
+++ WebRoot/assets/JS/homeScript.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
-    $('#submitButton').click(function() {
-        var playerName = $('#playerName').value;
-        console.log(playerName)
+    $('#submitButton').on('click',function(){
+        var datatoTransmit = 'uname=' + $('#playerName').val();
+
+        $.ajax({
+            url: "/ajax/register",
+            type: "POST",
+            data: datatoTransmit,
+            cache: false,
+            success: function(reternedData) {
+                //Call back
+                //Reload page? redirect?.....?
+            }
+        });
     });
 });
+
