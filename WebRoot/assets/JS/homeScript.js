038246bc565219c9e7e1fec5224a2d8953eb2038
Jordan Mackie <jmackie97@hotmail.com>
2016-11-19 23:36:57 +0000

    right actually fixed the redirect

--- WebRoot/assets/JS/homeScript.js
+++ WebRoot/assets/JS/homeScript.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
             data: datatoTransmit,
             cache: false,
             success: function(reternedData) {
-                window.location.replace("/view/map.php");
+                window.location.replace("/view/map");
             }
         });
     });
