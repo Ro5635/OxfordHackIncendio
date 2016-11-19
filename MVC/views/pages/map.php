19c7d85316d21718efec274298f3a5ae372df7ad
Stacey1212 <ai26@st-andrews.ac.uk>
2016-11-19 22:57:02 +0000

    Add map.php file

--- MVC/views/pages/map.php
+++ MVC/views/pages/map.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< Updated upstream
 Include the experimental html tag functions
 include($_SERVER['DOCUMENT_ROOT'].'..PHPIncludesLibrariesHTMLTagExperimental.php');
 
@@ -7,3 +8,17 @@ echo 'body';
 include($_SERVER['DOCUMENT_ROOT'].'..PHPIncludesViewspageshomeINC_TopPageGreeting.php');
 
 ?>
+=======
+//Include the experimental html tag functions
+include($_SERVER['DOCUMENT_ROOT'].'/../PHPIncludes/Libraries/HTMLTagExperimental.php');
+
+echo '<body>';
+
+include($_SERVER['DOCUMENT_ROOT'].'/../PHPIncludes/Views/pages/home/INC_TopPageGreeting.php');
+
+
+
+
+?>
+
+>>>>>>> Stashed changes
