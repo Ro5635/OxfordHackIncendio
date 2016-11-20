ab521f54e1af8f324053a7afa11b665f8a8db0d5
Jordan Mackie <jmackie97@hotmail.com>
2016-11-20 09:45:48 +0000

    attemot 2

--- MVC/views/pages/map.php
+++ MVC/views/pages/map.php
@@ -1,11 +1,25 @@
+<?php
+
+// Include the experimental html tag functions
+// include($_SERVER['DOCUMENT_ROOT'].'..PHPIncludesLibrariesHTMLTagExperimental.php');
+
+// echo '<body>';
+
+// include($_SERVER['DOCUMENT_ROOT'].'..PHPIncludesViewspageshomeINC_TopPageGreeting.php');
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
  <meta charset="utf-8">
  <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
- <title>FeatureLayer - 4.1</title>  
-<link rel="stylesheet" href="https://js.arcgis.com/4.1/esri/css/main.css">
- <script src="https://js.arcgis.com/4.1/"></script> 
+ <title>FeatureLayer - 4.1</title>
+
+ <link rel="stylesheet" href="https://js.arcgis.com/4.1/esri/css/main.css">
+ <script src="https://js.arcgis.com/4.1/"></script>
+
  <style>
    html,
    body,
@@ -15,40 +29,41 @@
      height: 100%;
      width: 100%;
    }
- </style> 
+ </style>
+
  <script>
-    $.get("https://oxfordhack.webaddressgoeshere.com/ajax/getPlayers", function(data, status){
-      alert("Data: "+data+"\nStatus: "+status);
+    $.get("/ajax/getPlayers", function(data){
+      alert("Data: "+data);
     });
  
    require([
          "esri/widgets/Track",
        "esri/Map",
-       "esri/views/MapView",       
-       "esri/layers/FeatureLayer",      
+       "esri/views/MapView",
+
+       "esri/layers/FeatureLayer",
+
        "dojo/domReady!"
      ],
-     
      function(
        Track, Map, MapView,
        FeatureLayer
-     ) 
-	{        
-       
+     ) {
 
-var featureLayer = new FeatureLayer({
+
+       var featureLayer = new FeatureLayer({
          url: "http://services.arcgis.com/Qo2anKIAMzIEkIJB/arcgis/rest/services/OxfordHackCampusDraft1/FeatureServer/0"
-       });   
+       });
 
-     
+
        var map = new Map({
          basemap: "streets"
-       });  
+       });
 
-      
        var view = new MapView({
          container: "viewDiv",
-         map: map,         
+         map: map,
+
          extent: { // autocasts as new Extent()
            xmin: -9177811,
            ymin: 4247000,
@@ -56,37 +71,30 @@ var featureLayer = new FeatureLayer({
            ymax: 4247784,
            spatialReference: 102100
          }
-       });        
+       });
 
-/********************
+       /********************
         * Add feature layer
-        ********************/        // Carbon storage of trees in Warren Wilson College.
+        ********************/
+
+       // Carbon storage of trees in Warren Wilson College.
     var track = new Track({
        view: view
-     });      
-     
+     });
+
      view.ui.add(track, "top-left");
-     map.add(featureLayer);  
-     
+     map.add(featureLayer);
+
      view.then(function() {
        track.start();
-     });      
-       
-       
+     });
+
      });
  </script>
-</head><body>
- <div id="viewDiv"></div>
-<<<<<<< HEAD
- <div id="listOfPlayers">
-    <ul>
-      <li></li>
-    </ul>
- </div>
+</head>
+
+<body>
  
 </body>
 
 </html>
-=======
-</body></html>
->>>>>>> 7e5de4ec8e8da5b61294462b06502fd1f9f5d072
