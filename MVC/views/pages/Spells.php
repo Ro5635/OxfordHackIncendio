8d4a0f9804ba1d647c0ece038f421902b36e2fad
RobertC <ro5635@gmail.com>
2016-11-20 14:26:44 +0000

    LastMinute.com

---
+++ MVC/views/pages/Spells.php
@@ -0,0 +1,36 @@
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-3">Harrys Spells</h1>
+    <p class="lead">Select Your Spell From The options Bellow:</p>
+  </div>
+</div>
+
+
+
+
+<div class="container-fluid">
+	 <div class="row ">
+<div class="card-deck-wrapper">
+  <div class="card-deck">
+    <div class="card">
+      <div class="card-block">
+        <h4 class="card-title">Spell 1</h4>
+        <p class="card-text">This is a description of the spell</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-block">
+        <h4 class="card-title">Spell 2</h4>
+        <p class="card-text">This is a description of the spell.</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-block">
+        <h4 class="card-title">Spell 3</h4>
+        <p class="card-text">This is a description of the spell</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
