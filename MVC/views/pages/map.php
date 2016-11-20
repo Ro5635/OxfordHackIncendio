d4e24c46104d76621cde5e55ebe3160aef6e574f
Stacey1212 <ai26@st-andrews.ac.uk>
2016-11-20 10:06:25 +0000

    Add footsteps

--- MVC/views/pages/map.php
+++ MVC/views/pages/map.php
@@ -15,11 +15,9 @@
 <head>
  <meta charset="utf-8">
  <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
- <title>FeatureLayer - 4.1</title>
-
- <link rel="stylesheet" href="https://js.arcgis.com/4.1/esri/css/main.css">
- <script src="https://js.arcgis.com/4.1/"></script>
-
+ <title>FeatureLayer - 4.1</title>  
+<link rel="stylesheet" href="https://js.arcgis.com/4.1/esri/css/main.css">
+ <script src="https://js.arcgis.com/4.1/"></script> 
  <style>
    html,
    body,
@@ -29,48 +27,34 @@
      height: 100%;
      width: 100%;
    }
- </style>
-
+ </style> 
  <script>
-    $.ajax({
-            
-	url: "/ajax/getPlayers",
-            type: "POST",
-            data: datatoTransmit,
-            cache: false,
-            success: function(reternedData) {
-                alert("Players:"+datatoTransmit);
-            }
-        });
- 
    require([
+     "esri/symbols/PictureMarkerSymbol",
          "esri/widgets/Track",
+           "esri/Graphic",
        "esri/Map",
-       "esri/views/MapView",
-
-       "esri/layers/FeatureLayer",
-
+       "esri/views/MapView",       
+       "esri/layers/FeatureLayer",      
        "dojo/domReady!"
      ],
-     function(
-       Track, Map, MapView,
-       FeatureLayer
-     ) {
+     
+     function(PictureMarkerSymbol, Track, Graphic, Map, MapView, FeatureLayer) {        
+       
 
-
-       var featureLayer = new FeatureLayer({
+var featureLayer = new FeatureLayer({
          url: "http://services.arcgis.com/Qo2anKIAMzIEkIJB/arcgis/rest/services/OxfordHackCampusDraft1/FeatureServer/0"
-       });
+       });   
 
-
+     
        var map = new Map({
          basemap: "streets"
-       });
+       });  
 
+      
        var view = new MapView({
          container: "viewDiv",
-         map: map,
-
+         map: map,         
          extent: { // autocasts as new Extent()
            xmin: -9177811,
            ymin: 4247000,
@@ -78,30 +62,34 @@
            ymax: 4247784,
            spatialReference: 102100
          }
-       });
+       });        
 
-       /********************
+/********************
         * Add feature layer
-        ********************/
-
-       // Carbon storage of trees in Warren Wilson College.
+        ********************/        
+// Carbon storage of trees in Warren Wilson College.
     var track = new Track({
-       view: view
-     });
-
+       view: view,
+       graphic: new Graphic({
+      symbol: new PictureMarkerSymbol("https://cdn.webaddressgoeshere.com/OxfordHack/media/footsteps_01.png", 25, 30)
+       })
+     });  
+     
+     
+     
      view.ui.add(track, "top-left");
-     map.add(featureLayer);
-
+     map.add(featureLayer);  
+     
      view.then(function() {
        track.start();
-     });
-
-     });
+     });      
+       
+       
+})
  </script>
 </head>
-
 <body>
- 
+ <div id="viewDiv">
+ </div>
 </body>
-
 </html>
