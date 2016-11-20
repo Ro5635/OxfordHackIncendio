845b0942ac02a4fcab58d05c24883b02421554de
RobertC <ro5635@gmail.com>
2016-11-20 04:02:43 +0000

    Add Script

--- MVC/controllers/pages_controller.php
+++ MVC/controllers/pages_controller.php
@@ -44,8 +44,7 @@
 
       $pageRequirements->add("title", 'Map');
 
-      $pageRequirements->add("js", ['https://js.arcgis.com/4.1/', 'assets/JS/Pusher.js','https://ajax.googleapis.com/ajax/libs/dojo/1.11.2/dojo/dojo.js', 'assets/JS/js.js', 'assets/JS/jsMap.js'] );
-
+      $pageRequirements->add("js", ['https://js.arcgis.com/4.1/', 'assets/JS/Pusher.js', 'https://js.pusher.com/3.2/pusher.min.js', 'https://ajax.googleapis.com/ajax/libs/dojo/1.11.2/dojo/dojo.js', 'assets/JS/js.js', 'assets/JS/jsMap.js'] );
 
 
       callStructural('header','std',$pageRequirements);
